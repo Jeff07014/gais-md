0495679370f0e15b67da93d339ba7f6d5b10cd8c
jeff07014 <gameplayer5032@gmai.com>
2021-04-15 00:40:57 +0800

    修改：     package.json 	修改：     public/index.html 	修改：     src/App.vue 	修改：     src/components/HelloWorld.vue 	package-lock.json 	patches/ 	public/materialize/ 	src/components/CollabEditor.vue 	src/components/Login.vue 	src/components/Markdown.vue 	src/components/TopBar.vue

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,61 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <transition-group name="fade">
+      <div key=1 v-if="connect">
+        <CollabEditor
+          :namespace="namespace"
+          :room="room">
+        </CollabEditor>
+      </div>
+      <div key=2 v-else>
+<!--
+      <input v-model="namespace">
+      <input v-model="room">
+      <button @click="connectEditor">Join Editor</button>
+-->
+        <Login v-on:getInput="getInput" />
+      </div>
+    </transition-group>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+//import TopBar from './components/TopBar.vue'
+import CollabEditor from './components/CollabEditor.vue'
+import Login from './components/Login.vue'
+// import Markdown from './components/Markdown.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    //Markdown,
+    CollabEditor,
+    Login,
+  },
+
+  data() {
+    return {
+      editorsConf: [],
+      namespace: "namespace1/project1",
+      room: "doc1",
+      connect: false,
+    }
+  },
+
+  methods: {
+    
+    getInput: function(ns, r) {
+      this.namespace = ns;
+      console.log(ns);
+      this.room = r;
+      console.log(r);
+      this.connect = true
+    }
   }
+  /*props: {
+    inputdata: String
+  }*/
 }
 </script>
 
@@ -25,4 +68,19 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 2s ease;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+}
+
+strong {
+  font-weight:700;
+}
+
 </style>
